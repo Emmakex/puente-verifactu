106651f118ea127506b30adbacb537358c57f5b1
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-15 10:10:08 +0200

    feat: export AEAT adapter

---
+++ packages/aeat-adapter/src/index.mjs
@@ -0,0 +1,7 @@
+export * from './adapter.mjs';
+export * from './constants.mjs';
+export * from './outbox.mjs';
+export * from './response.mjs';
+export * from './serialize.mjs';
+export * from './transport.mjs';
+export * from './xml.mjs';
